59b5b96bc4bf7a6126eb68545cca284a3bbe7bbd
GiuliaCastroo <maju.nogueir3.castr3@gmail.com>
2024-02-06 22:48:34 -0300

    Add revisão

---
+++ JS_REVISAO/revisao.js/revisao.js
@@ -0,0 +1,62 @@
+/*alert("Isso será exibido para o usuário");
+
+    /*document.write ("Jubs nasceu em  ")
+    document.write(2024 - 21)
+
+    /*document.write("A média das idades é: ");
+    document.write((20 + 10+ 30)/3) ;*/
+
+
+    /*Concatenação de string + inteiro*/
+
+    /*document.write ("Jules nasceu em :  " + (2024- 21) + "<br>"  ) ;
+    document.write("Emily nasceu em :  " + (2024 - 26) + "<br>");
+    document.write("Rose nasceu em :  " + (2024 - 16) + "<br>" );*/
+
+
+    /*variaveis
+
+    var ano = 2016;
+   
+
+    document.write("Flávio tem " + (ano - 1977) + " anos");
+    document.write("<br>");
+    document.write("Joaquim tem " + (ano - 1996) + " anos");
+    document.write("<br>");
+
+    ano = 2017;
+    document.write("Barney tem " + (ano - 1976) + " anos");
+    document.write("<br>");
+                
+    var idadeFlavio = 39;
+    var idadeJoaquim = 20;
+    var idadeBarney = 41;
+
+    var media= (idadeFlavio + idadeJoaquim + idadeBarney)/3;   
+    document.write('A média das idades é  ' + Math.round (media) );
+    document.write("<br>"); 
+
+    var nome = 'Flavio';
+
+    document.write ("A idade de   " + nome + ' é  ' + idadeFlavio)*/
+
+
+       /*funções*/
+
+
+
+function pulaLinha ()  {  /*Contruindo uma função para pular a linha*/ 
+        
+        document.write ("<br>");
+}  
+
+var ano = 2016;
+
+document.write("Flávio tem " + (ano - 1977) + " anos");
+pulaLinha();
+
+document.write("Joaquim tem " + (ano - 1996) + " anos");
+pulaLinha();
+
+ano = 2017;
+document.write("Barney tem " + (ano - 1976) + " anos");
